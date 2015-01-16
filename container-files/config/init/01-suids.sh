692256340bd3d468512346e83eb3ccb468024d8c
ryzy <marcin@ryzycki.com>
2015-01-16 13:50:31 +0000

    Revert "feat: suid for some commands used by 'www' user"
    This reverts commit 2f42ec03dff012c0fd5d63874c40a0832c274994.
    
    Seems like there's more disadvantages from this approach then benefits. Now even locally installed packages (e.g. in your project, node_modules) were owned by root which caused unexpected permission issues.

--- container-files/config/init/01-suids.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Add suid bit to some tools so 'www' user can use then w/o sudo command
-chmod u+s /usr/local/bin/gem 
-chmod u+s /usr/local/bin/node 
-
-# Change ownership of some tools used entirely by 'www' user
-chown www /usr/local/bin/composer
