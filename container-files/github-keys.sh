112d9637edabb48898a2f80158755d07a7b7f0e9
ryzy <marcin@ryzycki.com>
2016-08-04 21:02:13 +0200

    Improve GitHub API rate limit warning
    This fixes nasty `[: -eq: unary operator expected` error
    plus adds another warning about empty selection of keys for provided
    user.
    
    Relates: #10
    Relates: #11

--- container-files/github-keys.sh
+++ container-files/github-keys.sh
@@ -1,31 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #
 # Source: https://github.com/rtlong, https://gist.github.com/rtlong/6790049
 # Usage: /github-keys.sh | bash -s <github username>
 #
 
-IFS="$(printf '\n\t')"
-
 user=$1
-api_call=$(curl -v https://api.github.com/users/$user/keys 2>&1)
-rate_limit=$(echo $api_call | grep -o -E 'X-RateLimit-Remaining:\s\d+' | grep -o -E '\d+')
-keys=$(echo $api_call | grep -o -E 'ssh-\w+\s+[^\"]+')
+api_response=$(curl -sSLi https://api.github.com/users/$user/keys)
+remaining_rate_limit=$(echo "$api_response" | grep -o -E "X-RateLimit-Remaining:\s[0-9]+" | awk '{print $2}')
+keys=$(echo $api_response | grep -o -E 'ssh-\w+\s+[^\"]+')
 
-if [ $rate_limit -eq 0 ];
-  then
-    echo "WARNING: Github API rate limit exceeded. No key(s) added to `whoami` account."
+if [ $remaining_rate_limit -eq 0 ]; then
+  echo "WARNING: Github API rate limit exceeded. No key(s) added to `whoami` account."
+else
+  if [ -z "$keys" ]; then
+    echo "WARNING: GitHub doesn't have any keys for '$user' user."
   else
     echo "Importing $user's GitHub pub key(s) to `whoami` account..."
 
-    mkdir -p ~/.ssh
-    if ! [[ -f ~/.ssh/authorized_keys ]]; then
-      echo "Creating new ~/.ssh/authorized_keys"
-      touch ~/.ssh/authorized_keys
-    fi
+    [ -d ~/.ssh ] || mkdir ~/.ssh
+    [ -f ~/.ssh/authorized_keys ] || touch ~/.ssh/authorized_keys
 
     for key in $keys; do
       echo "Imported GitHub $user key: $key"
       grep -q "$key" ~/.ssh/authorized_keys || echo "$key ${user}@github" >> ~/.ssh/authorized_keys
     done
+  fi
 fi
